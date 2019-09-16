2d1c5a5f9e95a485ece717fbfaad6485734399af
Dmitry Kann <yktooo@gmail.com>
2019-09-16 12:08:18 +0200

    Implement history navigation, make linking to abbrs possible

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     var timer;
     var reCleanup = /[^a-zа-яё0-9]+/g;
+    var abbrRows = $('#abbr-table tr');
+    var searchBox = $('#search-text');
 
     // Remove search timer, if any
     function clearTimer() {
@@ -18,44 +20,85 @@ $(document).ready(function () {
         }
     }
 
-    // Perform abbreviation search
-    function doSearch() {
+    /**
+     * Perform abbreviation search
+     * @param pattern string Optional search pattern. If not given, the text in the search input is used.
+     * @param noHistory bool Whether to store the search in browser history.
+     */
+    function doSearch(pattern, noHistory) {
+        // Remove any pending timer
         clearTimer();
-        var search = $('#search-text').val().toLowerCase().replace(reCleanup, '');
-        var cnt = 0;
-        $('#abbr-table tr').each(function () {
-            var abbr = $('td', this).text().toLowerCase().replace(reCleanup, '');
-            if (search.length > 0 && abbr.includes(search)) {
-                setVisible($(this), true);
-                cnt++;
-            } else {
-                setVisible($(this), false);
-            }
-        });
 
-        // Adjust result count panels
-        setVisible($('#results-none'),    search.length > 0 && cnt === 0);
-        setVisible($('#results-present'), search.length > 0 && cnt > 0);
+        // If a pattern is given, push it to the search input
+        if (pattern) {
+            searchBox.val(pattern);
+        // Otherwise use whatever in the search box
+        } else {
+            pattern = searchBox.val();
+        }
+
+        // Clean up the pattern
+        var cleanPattern = pattern.toLowerCase().replace(reCleanup, '');
+        var hasPattern = cleanPattern.length > 0;
+
+        // Process the table row-by-row
+        var cnt = 0;
+        if (hasPattern) {
+            abbrRows.each(function () {
+                var abbr = $('td', this).text().toLowerCase().replace(reCleanup, '');
+                if (abbr.includes(cleanPattern)) {
+                    setVisible($(this), true);
+                    cnt++;
+                } else {
+                    setVisible($(this), false);
+                }
+            });
+        }
+
+        // Adjust result display
         $('#result-count').html(cnt);
+        setVisible($('#results-none'), hasPattern && cnt === 0);
+        setVisible($('#results'),      hasPattern && cnt > 0);
+
+        // Update page URL
+        hasPattern && !noHistory && history.pushState(null, pattern, '#' + pattern);
+    }
+
+    /**
+     * Perform search based on the current URL hash
+     */
+    function searchCurHash() {
+        if (window.location.hash.length > 1) {
+            doSearch(decodeURIComponent(window.location.hash.substring(1)), true);
+        }
     }
 
     // Add a search pattern change listener
-    $('#search-text').on('keyup change paste search', function (e) {
+    searchBox.on('keyup change paste', function (e) {
         clearTimer();
         timer = setTimeout(doSearch, 500);
     });
-
-    // Init some examples
-    var rows = $('#abbr-table tr');
-    var exList = $('#search-help');
-    for (var i = 0; i <=3; i++) {
-        var idx = Math.floor(Math.random() * rows.length);
-        var row = rows.eq(idx);
-        exList.append('<a class="search-example" href="#">' + $('td:first-child', row).text() + '</a>')
-    }
-    $('.search-example').click(function (event) {
-        event.preventDefault();
-        $('#search-text').val($(this).text());
+    searchBox.on('search', function () {
         doSearch();
     });
+
+    // Add a history change listener
+    $(window).on('popstate', searchCurHash);
+
+    // Init some examples
+    var exList = $('#search-help');
+    for (var i = 0; i <=3; i++) {
+        var idx = Math.floor(Math.random() * abbrRows.length);
+        var row = abbrRows.eq(idx);
+        exList.append('<a class="search-example" href="#">' + $('td:first-child', row).text() + '</a>')
+    }
+
+    // Init example click handler
+    $('.search-example').click(function (event) {
+        event.preventDefault();
+        doSearch($(this).text());
+    });
+
+    // Perform initial search if the URL contains a hash part
+    searchCurHash();
 });
