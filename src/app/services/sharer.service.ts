300074dc933fb138d3999376abf2f584089b628d
Dmitry Kann <yktooo@gmail.com>
2022-09-16 12:52:49 +0200

    Revert "sharing: %-encode trailing dot in URL"
    This reverts commit f78bb82574a83261c03a78768cfc1f0d81135b70.

--- src/app/services/sharer.service.ts
+++ src/app/services/sharer.service.ts
@@ -104,16 +104,9 @@ export class SharerService {
      * @param title Title text.
      */
     getShareUrl(sharer: Sharer, url: string, title: string): SafeUrl {
-        // Encode the URL
-        url = encodeURIComponent(url)
-        // If it ends with a dot, percent-encode that trailing dot to prevent parsing errors while sharing
-        if (url.endsWith('.')) {
-            url = url.slice(0, -1) + '%2E';
-        }
-        // Make a safe sharing URL
         return this.sanitizer.bypassSecurityTrustUrl(
             sharer.shareUrl
-                .replace('\tu', url)
+                .replace('\tu', encodeURIComponent(url))
                 .replace('\tt', encodeURIComponent(title)));
     }
 
