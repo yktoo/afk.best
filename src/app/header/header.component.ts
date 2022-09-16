f0e320af9e9882ee5537c6f1d83a4544da412b66
Dmitry Kann <yktooo@gmail.com>
2022-09-16 11:12:06 +0200

    language switcher: keep the same route/params on switch

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, LOCALE_ID } from '@angular/core';
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
     selector: 'app-header',
@@ -17,5 +18,13 @@ export class HeaderComponent {
 
     constructor(
         @Inject(LOCALE_ID) readonly locale: string,
+        @Inject(DOCUMENT) private readonly doc: Document,
     ) {}
+
+    /**
+     * Returns the current URL but for the given language version of the app.
+     */
+    getLangUrl(lang: string): string {
+        return this.doc.location.href.replace(`/${this.locale}/`, `/${lang}/`);
+    }
 }
