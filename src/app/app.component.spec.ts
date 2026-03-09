95d1cab14b62d4803ae559d6624da18e032d8a19
Dmitry Kann <yktooo@gmail.com>
2026-03-09 10:50:58 +0100

    Angular 21, bump deps, ditch ng-mocks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
-import { MockComponents } from 'ng-mocks';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 describe('AppComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [AppComponent, RouterModule.forRoot([]), MockComponents(HeaderComponent, FooterComponent)],
+            imports: [AppComponent, RouterModule.forRoot([])],
                 providers: [provideZonelessChangeDetection()],
         })
             .compileComponents();
